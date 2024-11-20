21cdaaee42fade21f43014c983bb650352f61926
Fred Morcos <fm@fredmorcos.com>
2024-11-21 00:29:49 +0100

    fix: quote file names that contain spaces when printing them to stdout (#51)

--- crates/television-channels/src/entry.rs
+++ crates/television-channels/src/entry.rs
@@ -105,6 +105,10 @@ impl Entry {
 
     pub fn stdout_repr(&self) -> String {
         let mut repr = self.name.clone();
+        if repr.contains(|c| char::is_ascii_whitespace(&c)) {
+            repr.insert(0, '\'');
+            repr.push('\'');
+        }
         if let Some(line_number) = self.line_number {
             repr.push_str(&format!(":{line_number}"));
         }
