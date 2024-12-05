36db70498fe43d77030063c23b6583f0d4db1658
Bertrand Chardon <bertrand.chardon@doctrine.fr>
2024-12-05 14:00:36 +0100

    WIP refactoring of tv screen

--- crates/television-screen/src/metadata.rs
+++ crates/television-screen/src/metadata.rs
@@ -1,14 +1,13 @@
 use std::fmt::Display;
 
+use crate::mode::{mode_color, Mode};
 use ratatui::{
     layout::Constraint,
     style::{Color, Style},
     text::Span,
     widgets::{Cell, Row, Table},
 };
-
-use crate::mode::{mode_color, Mode};
-use tv::television::Television;
+use television_channels::channels::UnitChannel;
 
 const METADATA_FIELD_NAME_COLOR: Color = Color::DarkGray;
 const METADATA_FIELD_VALUE_COLOR: Color = Color::Gray;
@@ -23,101 +22,102 @@ impl Display for Mode {
     }
 }
 
-impl Television {
-    pub fn build_metadata_table<'a>(&self) -> Table<'a> {
-        let version_row = Row::new(vec![
-            Cell::from(Span::styled(
-                "version: ",
-                Style::default().fg(METADATA_FIELD_NAME_COLOR),
-            )),
-            Cell::from(Span::styled(
-                env!("CARGO_PKG_VERSION"),
-                Style::default().fg(METADATA_FIELD_VALUE_COLOR),
-            )),
-        ]);
+pub fn build_metadata_table<'a>(
+    mode: Mode,
+    current_channel: UnitChannel,
+) -> Table<'a> {
+    let version_row = Row::new(vec![
+        Cell::from(Span::styled(
+            "version: ",
+            Style::default().fg(METADATA_FIELD_NAME_COLOR),
+        )),
+        Cell::from(Span::styled(
+            env!("CARGO_PKG_VERSION"),
+            Style::default().fg(METADATA_FIELD_VALUE_COLOR),
+        )),
+    ]);
 
-        let target_triple_row = Row::new(vec![
-            Cell::from(Span::styled(
-                "target triple: ",
-                Style::default().fg(METADATA_FIELD_NAME_COLOR),
-            )),
-            Cell::from(Span::styled(
-                env!("VERGEN_CARGO_TARGET_TRIPLE"),
-                Style::default().fg(METADATA_FIELD_VALUE_COLOR),
-            )),
-        ]);
+    let target_triple_row = Row::new(vec![
+        Cell::from(Span::styled(
+            "target triple: ",
+            Style::default().fg(METADATA_FIELD_NAME_COLOR),
+        )),
+        Cell::from(Span::styled(
+            env!("VERGEN_CARGO_TARGET_TRIPLE"),
+            Style::default().fg(METADATA_FIELD_VALUE_COLOR),
+        )),
+    ]);
 
-        let build_row = Row::new(vec![
-            Cell::from(Span::styled(
-                "build: ",
-                Style::default().fg(METADATA_FIELD_NAME_COLOR),
-            )),
-            Cell::from(Span::styled(
-                env!("VERGEN_RUSTC_SEMVER"),
-                Style::default().fg(METADATA_FIELD_VALUE_COLOR),
-            )),
-            Cell::from(Span::styled(
-                " (",
-                Style::default().fg(METADATA_FIELD_NAME_COLOR),
-            )),
-            Cell::from(Span::styled(
-                env!("VERGEN_BUILD_DATE"),
-                Style::default().fg(METADATA_FIELD_VALUE_COLOR),
-            )),
-            Cell::from(Span::styled(
-                ")",
-                Style::default().fg(METADATA_FIELD_NAME_COLOR),
-            )),
-        ]);
+    let build_row = Row::new(vec![
+        Cell::from(Span::styled(
+            "build: ",
+            Style::default().fg(METADATA_FIELD_NAME_COLOR),
+        )),
+        Cell::from(Span::styled(
+            env!("VERGEN_RUSTC_SEMVER"),
+            Style::default().fg(METADATA_FIELD_VALUE_COLOR),
+        )),
+        Cell::from(Span::styled(
+            " (",
+            Style::default().fg(METADATA_FIELD_NAME_COLOR),
+        )),
+        Cell::from(Span::styled(
+            env!("VERGEN_BUILD_DATE"),
+            Style::default().fg(METADATA_FIELD_VALUE_COLOR),
+        )),
+        Cell::from(Span::styled(
+            ")",
+            Style::default().fg(METADATA_FIELD_NAME_COLOR),
+        )),
+    ]);
 
-        let current_dir_row = Row::new(vec![
-            Cell::from(Span::styled(
-                "current directory: ",
-                Style::default().fg(METADATA_FIELD_NAME_COLOR),
-            )),
-            Cell::from(Span::styled(
-                std::env::current_dir()
-                    .expect("Could not get current directory")
-                    .display()
-                    .to_string(),
-                Style::default().fg(METADATA_FIELD_VALUE_COLOR),
-            )),
-        ]);
+    let current_dir_row = Row::new(vec![
+        Cell::from(Span::styled(
+            "current directory: ",
+            Style::default().fg(METADATA_FIELD_NAME_COLOR),
+        )),
+        Cell::from(Span::styled(
+            std::env::current_dir()
+                .expect("Could not get current directory")
+                .display()
+                .to_string(),
+            Style::default().fg(METADATA_FIELD_VALUE_COLOR),
+        )),
+    ]);
 
-        let current_channel_row = Row::new(vec![
-            Cell::from(Span::styled(
-                "current channel: ",
-                Style::default().fg(METADATA_FIELD_NAME_COLOR),
-            )),
-            Cell::from(Span::styled(
-                self.current_channel().to_string(),
-                Style::default().fg(METADATA_FIELD_VALUE_COLOR),
-            )),
-        ]);
+    let current_channel_row = Row::new(vec![
+        Cell::from(Span::styled(
+            "current channel: ",
+            Style::default().fg(METADATA_FIELD_NAME_COLOR),
+        )),
+        Cell::from(Span::styled(
+            current_channel.to_string(),
+            Style::default().fg(METADATA_FIELD_VALUE_COLOR),
+        )),
+    ]);
 
-        let current_mode_row = Row::new(vec![
-            Cell::from(Span::styled(
-                "current mode: ",
-                Style::default().fg(METADATA_FIELD_NAME_COLOR),
-            )),
-            Cell::from(Span::styled(
-                self.mode.to_string(),
-                Style::default().fg(mode_color(self.mode)),
-            )),
-        ]);
+    let current_mode_row = Row::new(vec![
+        Cell::from(Span::styled(
+            "current mode: ",
+            Style::default().fg(METADATA_FIELD_NAME_COLOR),
+        )),
+        Cell::from(Span::styled(
+            mode.to_string(),
+            Style::default().fg(mode_color(mode)),
+        )),
+    ]);
 
-        let widths = vec![Constraint::Fill(1), Constraint::Fill(2)];
+    let widths = vec![Constraint::Fill(1), Constraint::Fill(2)];
 
-        Table::new(
-            vec![
-                version_row,
-                target_triple_row,
-                build_row,
-                current_dir_row,
-                current_channel_row,
-                current_mode_row,
-            ],
-            widths,
-        )
-    }
+    Table::new(
+        vec![
+            version_row,
+            target_triple_row,
+            build_row,
+            current_dir_row,
+            current_channel_row,
+            current_mode_row,
+        ],
+        widths,
+    )
 }
