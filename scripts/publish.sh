8fbcb42b9242b6a1c1b98b51cbc3c579537b46b7
Alexandre Pasmantier <alex.pasmant@gmail.com>
2024-12-15 14:08:08 +0100

    chore(version): bump workspace to 0.7.0

--- scripts/publish.sh
+++ scripts/publish.sh
@@ -1,11 +1,13 @@
 cargo publish -p television-derive
-sleep 20
+sleep 10
 cargo publish -p television-fuzzy
-sleep 20
+sleep 10
 cargo publish -p television-utils
-sleep 20
+sleep 10
 cargo publish -p television-channels
-sleep 20
+sleep 10
 cargo publish -p television-previewers
-sleep 20
+sleep 10
+cargo publish -p television-screen
+sleep 10
 cargo publish -p television
