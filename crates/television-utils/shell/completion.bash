db353067a2ce908f15776d0158dfab273f49174c
Alexandre Pasmantier <alex.pasmant@gmail.com>
2024-12-30 17:08:22 +0100

    feat(shell): add bash support for smart autocomplete and shell history

---
+++ crates/television-utils/shell/completion.bash
@@ -0,0 +1,24 @@
+function tv_smart_autocomplete() {
+  local current_prompt="${READLINE_LINE:0:$READLINE_POINT}"
+
+  local output=$(tv --autocomplete-prompt "$current_prompt")
+
+  if [[ -n $output ]]; then
+    READLINE_LINE=$current_prompt$output
+    READLINE_POINT=${#READLINE_LINE}
+  fi
+}
+
+function tv_shell_history() {
+  local current_prompt="${READLINE_LINE:0:$READLINE_POINT}"
+
+  local output=$(tv bash-history --input "$current_prompt")
+
+  if [[ -n $output ]]; then
+    READLINE_LINE=$output
+    READLINE_POINT=${#READLINE_LINE}
+  fi
+}
+
+bind -x '"\C-t": tv_smart_autocomplete'
+bind -x '"\C-r": tv_shell_history'
