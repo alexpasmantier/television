3ff6ab553fef0109344376bdbb0dc4af6dcda596
Tulip Blossom <tulilirockz@outlook.com>
2025-01-01 01:36:41 -0300

    fix: automatically create configuration directory if it doesnt exist
    I've noticed that the latest release (0.85.0) failed to start up if `$HOME/.config/television` doesnt exist. This should force the directory creation if the user doesn't have it.
    
    Before:
    (screenshot)
    
    After:
    (screenshot)

--- crates/television/config.rs
+++ crates/television/config.rs
@@ -83,6 +83,8 @@ impl Config {
         // initialize the config builder
         let data_dir = get_data_dir();
         let config_dir = get_config_dir();
+        std::fs::create_dir_all(config_dir.clone()).expect("Failed creating configuration directory");
+
         let mut builder = config::Config::builder()
             .set_default("data_dir", data_dir.to_str().unwrap())?
             .set_default("config_dir", config_dir.to_str().unwrap())?
