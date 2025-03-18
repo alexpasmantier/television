e6751f5847e10b8db4c1aca5d90880c45ffef433
alexpasmantier <alex.pasmant@gmail.com>
2025-03-18 01:51:17 +0100

    chore: bump to 0.10.10

--- build.rs
+++ build.rs
@@ -8,21 +8,24 @@ fn build_man_page() -> std::io::Result<()> {
     let out_dir = PathBuf::from(
         std::env::var_os("OUT_DIR").ok_or(std::io::ErrorKind::NotFound)?,
     );
-    std::fs::create_dir_all(&out_dir)?;
     let cmd = Cli::command();
     let man = clap_mangen::Man::new(cmd);
     let mut buffer = Vec::<u8>::default();
     man.render(&mut buffer)?;
 
-    let out_path = out_dir.join("tv.1");
+    let out_path = out_dir
+        .ancestors()
+        .nth(4)
+        .unwrap()
+        .join("assets")
+        .join("tv.1");
+    std::fs::create_dir_all(out_path.parent().unwrap())?;
     std::fs::write(&out_path, &buffer)?;
-    eprintln!("Wrote man page to {out_path:?}");
-    std::fs::write(PathBuf::from("./man").join("tv.1"), &buffer)?;
-    eprintln!("Wrote man page to ./man directory.");
     Ok(())
 }
 
 fn main() {
     println!("cargo::rerun-if-changed=television/cli/args.rs");
+    println!("cargo::rerun-if-changed=build.rs");
     build_man_page().expect("Failed to generate man page.");
 }
