3b5771000622ee02bba414cadd4419d466fd8116
alexandre pasmantier <alex.pasmant@gmail.com>
2025-06-16 01:43:18 +0200

    test(e2e): more end to end tests

--- tests/e2e.rs
+++ tests/e2e.rs
@@ -1,97 +1,260 @@
-use std::path::Path;
+use std::thread::sleep;
+use std::time::Duration;
 
-use rexpect::error::Error;
-use rexpect::process::wait::WaitStatus;
-use rexpect::spawn;
-use television::channels::prototypes::ChannelPrototype;
-use tempfile::TempDir;
+use portable_pty::{CommandBuilder, PtySize, native_pty_system};
 
-#[allow(dead_code)]
-fn setup_config(content: &str) -> TempDir {
-    let temp_dir = tempfile::tempdir().unwrap();
-    let config_path = temp_dir.path().join("config.toml");
-    std::fs::write(&config_path, content).unwrap();
-    temp_dir
+const DEFAULT_CONFIG_FILE: &str = "./.config/config.toml";
+#[cfg(unix)]
+const DEFAULT_CABLE_DIR: &str = "./cable/unix";
+#[cfg(windows)]
+const DEFAULT_CABLE_DIR: &str = "./cable/windows";
+
+const DEFAULT_PTY_SIZE: PtySize = PtySize {
+    rows: 20,
+    cols: 60,
+    pixel_width: 0,
+    pixel_height: 0,
+};
+
+const DEFAULT_TIMEOUT: Duration = Duration::from_millis(400);
+
+fn assert_exit_ok(
+    child: &mut Box<dyn portable_pty::Child + Send + Sync + 'static>,
+    timeout: Duration,
+) {
+    let now = std::time::Instant::now();
+    while now.elapsed() < timeout {
+        match child.try_wait() {
+            Ok(Some(status)) => {
+                assert!(
+                    status.success(),
+                    "Process exited with non-zero status: {:?}",
+                    status
+                );
+                return;
+            }
+            Ok(None) => {
+                // Process is still running, continue waiting
+                sleep(Duration::from_millis(50));
+            }
+            Err(e) => {
+                panic!("Error waiting for process: {}", e);
+            }
+        }
+    }
+    panic!("Process did not exit in time");
 }
 
-fn setup_cable_channels<D>(channels: Vec<&str>, cable_dir: D)
-where
-    D: AsRef<Path>,
-{
-    let cable_dir = cable_dir.as_ref();
-    std::fs::create_dir_all(cable_dir).unwrap();
-    for channel in channels {
-        let name = toml::from_str::<ChannelPrototype>(channel)
-            .unwrap()
-            .metadata
-            .name;
-        let channel_path = cable_dir.join(format!("{}.toml", name));
-        std::fs::write(&channel_path, channel).unwrap();
+#[test]
+fn tv_version() {
+    let pty_system = native_pty_system();
+    // Create a new pty
+    let pair = pty_system.openpty(DEFAULT_PTY_SIZE).unwrap();
+
+    // Spawn a tv process in the pty
+    let mut cmd = CommandBuilder::new("./target/debug/tv");
+    cmd.cwd(std::env::current_dir().unwrap());
+    cmd.args(["--version"]);
+    let mut child: Box<dyn portable_pty::Child + Send + Sync + 'static> =
+        pair.slave.spawn_command(cmd).unwrap();
+    sleep(Duration::from_millis(200));
+
+    // Read the output from the pty
+    let mut buf = [0; 512];
+    let mut reader = pair.master.try_clone_reader().unwrap();
+    let _ = reader.read(&mut buf).unwrap();
+    let output = String::from_utf8_lossy(&buf);
+    assert!(output.contains("television"));
+
+    assert_exit_ok(&mut child, DEFAULT_TIMEOUT);
+}
+
+#[test]
+fn tv_help() {
+    let pty_system = native_pty_system();
+    // Create a new pty
+    let pair = pty_system.openpty(DEFAULT_PTY_SIZE).unwrap();
+
+    // Spawn a tv process in the pty
+    let mut cmd = CommandBuilder::new("./target/debug/tv");
+    cmd.cwd(std::env::current_dir().unwrap());
+    cmd.args(["--help"]);
+    let mut child = pair.slave.spawn_command(cmd).unwrap();
+    sleep(Duration::from_millis(200));
+
+    // Read the output from the pty
+    let mut buf = [0; 512];
+    let mut reader = pair.master.try_clone_reader().unwrap();
+    let _ = reader.read(&mut buf).unwrap();
+    let output = String::from_utf8_lossy(&buf);
+    assert!(output.contains("A cross-platform"));
+
+    assert_exit_ok(&mut child, DEFAULT_TIMEOUT);
+}
+
+#[test]
+fn tv_list_channels() {
+    let pty_system = native_pty_system();
+    // Create a new pty
+    let pair = pty_system.openpty(DEFAULT_PTY_SIZE).unwrap();
+
+    // Spawn a tv process in the pty
+    let mut cmd = CommandBuilder::new("./target/debug/tv");
+    cmd.cwd(std::env::current_dir().unwrap());
+    cmd.args([
+        "--cable-dir",
+        DEFAULT_CABLE_DIR,
+        "--config-file",
+        DEFAULT_CONFIG_FILE,
+        "list-channels",
+    ]);
+    let mut child = pair.slave.spawn_command(cmd).unwrap();
+    sleep(Duration::from_millis(200));
+
+    // Read the output from the pty
+    let mut buf = [0; 512];
+    let mut reader = pair.master.try_clone_reader().unwrap();
+    let _ = reader.read(&mut buf).unwrap();
+    let output = String::from_utf8_lossy(&buf);
+    assert!(output.contains("files"), "Output: {}", output);
+    assert!(output.contains("dirs"), "Output: {}", output);
+
+    assert_exit_ok(&mut child, DEFAULT_TIMEOUT);
+}
+
+#[test]
+fn tv_init_zsh() {
+    let pty_system = native_pty_system();
+    // Create a new pty
+    let pair = pty_system.openpty(DEFAULT_PTY_SIZE).unwrap();
+
+    // Spawn a tv process in the pty
+    let mut cmd = CommandBuilder::new("./target/debug/tv");
+    cmd.cwd(std::env::current_dir().unwrap());
+    cmd.args([
+        "--cable-dir",
+        DEFAULT_CABLE_DIR,
+        "--config-file",
+        DEFAULT_CONFIG_FILE,
+        "init",
+        "zsh",
+    ]);
+    let mut child = pair.slave.spawn_command(cmd).unwrap();
+    sleep(Duration::from_millis(200));
+
+    assert_exit_ok(&mut child, DEFAULT_TIMEOUT);
+}
+
+/// Creates a command to run `tv` with the repo's config and cable directory
+/// using the cable directory as cwd.
+fn tv_command(channel: &str) -> CommandBuilder {
+    let mut cmd = CommandBuilder::new("./target/debug/tv");
+    cmd.cwd(std::env::current_dir().unwrap());
+    cmd.args([
+        "--cable-dir",
+        DEFAULT_CABLE_DIR,
+        "--config-file",
+        DEFAULT_CONFIG_FILE,
+        channel,
+    ]);
+    cmd
+}
+
+#[test]
+fn tv_ctrl_c() {
+    let pty_system = native_pty_system();
+    // Create a new pty
+    let pair = pty_system.openpty(DEFAULT_PTY_SIZE).unwrap();
+
+    // Spawn a tv process in the pty
+    let mut child = pair.slave.spawn_command(tv_command("files")).unwrap();
+    sleep(Duration::from_millis(200));
+
+    // Send Ctrl-C to the process
+    let mut writer = pair.master.take_writer().unwrap();
+    writeln!(writer, "\x03").unwrap(); // Ctrl-C
+
+    // Check if the child process exited with a timeout
+    assert_exit_ok(&mut child, DEFAULT_TIMEOUT);
+}
+
+#[test]
+fn tv_remote_control() {
+    let pty_system = native_pty_system();
+    // Create a new pty
+    let pair = pty_system.openpty(DEFAULT_PTY_SIZE).unwrap();
+
+    // Spawn a tv process in the pty
+    let mut child = pair.slave.spawn_command(tv_command("files")).unwrap();
+    sleep(Duration::from_millis(200));
+
+    // Send Ctrl-T to the process (open remote control mode)
+    let mut writer = pair.master.take_writer().unwrap();
+    writeln!(writer, "\x14").unwrap(); // Ctrl-T
+    sleep(Duration::from_millis(200));
+
+    let mut buf = [0; 5096]; // Buffer size for reading output
+    let mut reader = pair.master.try_clone_reader().unwrap();
+    let _ = reader.read(&mut buf).unwrap();
+
+    let s = String::from_utf8_lossy(&buf);
+
+    assert!(s.contains("Remote Control"));
+
+    // Send Ctrl-c to exit remote control mode
+    writeln!(writer, "\x03").unwrap();
+    sleep(Duration::from_millis(200));
+    // resend Ctrl-c to finally exit
+    writeln!(writer, "\x03").unwrap();
+    sleep(Duration::from_millis(200));
+
+    assert_exit_ok(&mut child, DEFAULT_TIMEOUT);
+}
+
+macro_rules! test_channel {
+    ($($name:ident: $channel_name:expr,)*) => {
+    $(
+        #[test]
+        fn $name() {
+            let pty_system = native_pty_system();
+            // Create a new pty
+            let pair = pty_system.openpty(DEFAULT_PTY_SIZE).unwrap();
+            let cmd = tv_command(&$channel_name);
+
+            let mut child = pair.slave.spawn_command(cmd).unwrap();
+            sleep(Duration::from_millis(300));
+
+            // Read the output from the pty
+            let mut buf = [0; 5096];
+            let mut reader = pair.master.try_clone_reader().unwrap();
+            let _ = reader.read(&mut buf).unwrap();
+            let output = String::from_utf8_lossy(&buf);
+
+            assert!(
+                output.contains(&$channel_name),
+                "Unable to find channel name (\"{}\") in output: {}",
+                &$channel_name,
+                output
+            );
+
+            // Send Ctrl-C to the process
+            let mut writer = pair.master.take_writer().unwrap();
+            writeln!(writer, "\x03").unwrap(); // Ctrl-C
+            sleep(Duration::from_millis(200));
+
+            assert_exit_ok(&mut child, DEFAULT_TIMEOUT);
+        }
+    )*
     }
 }
 
-#[test]
-fn tv_version() -> Result<(), Error> {
-    let mut p = spawn("./target/debug/tv --version", Some(500))?;
-    p.exp_regex("television [0-9]+\\.[0-9]+\\.[0-9]+")?;
-
-    Ok(())
-}
-
-#[test]
-fn tv_help() -> Result<(), Error> {
-    let mut p = spawn("./target/debug/tv --help", Some(500))?;
-    p.exp_regex("A cross-platform")?;
-
-    Ok(())
-}
-
-const BASIC_FILE_CHANNEL: &str = r#"
-    [metadata]
-    name = "files"
-
-    [source]
-    command = "fd -t f"
-"#;
-
-const BASIC_DIR_CHANNEL: &str = r#"
-    [metadata]
-    name = "dirs"
-
-    [source]
-    command = "fd -t d"
-"#;
-
-#[test]
-fn tv_list_channels() -> Result<(), Error> {
-    let temp_dir = TempDir::new().unwrap();
-    setup_cable_channels(
-        vec![BASIC_FILE_CHANNEL, BASIC_DIR_CHANNEL],
-        &temp_dir,
-    );
-
-    let mut p = spawn(
-        &format!(
-            "./target/debug/tv --cable-dir {} list-channels ",
-            temp_dir.path().display()
-        ),
-        Some(500),
-    )?;
-    p.exp_regex("files")?;
-    p.exp_regex("dirs")?;
-
-    Ok(())
-}
-
-#[test]
-fn tv_init_zsh() -> Result<(), Error> {
-    let p = spawn("./target/debug/tv init zsh", Some(500))?;
-    // check that the process exits successfully
-    if let Ok(w) = p.process.wait() {
-        assert_eq!(w, WaitStatus::Exited(w.pid().unwrap(), 0));
-    } else {
-        panic!("Failed to wait for process");
-    }
-
-    Ok(())
+test_channel! {
+    test_channel_files: "files",
+    test_channel_dirs: "dirs",
+    test_channel_env: "env",
+    test_channel_git_log: "git-log",
+    test_channel_git_reflog: "git-reflog",
+    test_channel_git_branch: "git-branch",
+    test_channel_text: "text",
+    test_channel_diff: "git-diff",
 }
