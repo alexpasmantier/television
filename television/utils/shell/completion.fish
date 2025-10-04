f13235cec2a8a77d972bcebce4b13e7a3a673631
Gaurav Kumar <gauravkumar37@gmail.com>
2025-10-04 18:57:21 +0530

    Redirect output to /dev/tty for autocomplete commands

--- television/utils/shell/completion.fish
+++ television/utils/shell/completion.fish
@@ -98,7 +98,7 @@ function tv_smart_autocomplete
     # move to the next line so that the prompt is not overwritten
     printf "\n"
 
-    if set -l result (tv $dir --autocomplete-prompt "$current_prompt" --input $tv_query --inline)
+    if set -l result (tv $dir --autocomplete-prompt "$current_prompt" --input $tv_query --inline >/dev/tty)
         # Remove last token from commandline.
         commandline -t ''
 
@@ -127,7 +127,7 @@ function tv_shell_history
     # move to the next line so that the prompt is not overwritten
     printf "\n"
 
-    set -l output (tv fish-history --input "$current_prompt" --inline)
+    set -l output (tv fish-history --input "$current_prompt" --inline >/dev/tty)
 
     if test -n "$output"
         commandline -r "$output"
