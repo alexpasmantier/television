7ca329492ea04516a31538211e8ceb955d95d28f
Alex Pasmantier <alex.pasmant@gmail.com>
2025-11-21 14:06:28 +0100

    perf: add rendering benchmark

--- benches/main.rs
+++ benches/main.rs
@@ -2,9 +2,10 @@ use criterion::criterion_main;
 
 pub mod main {
     pub mod load_candidates;
+    pub mod render;
     pub mod ui;
 }
 
 pub use main::*;
 
-criterion_main!(ui::benches, load_candidates::benches,);
+criterion_main!(ui::benches, load_candidates::benches, render::benches,);
