c4774027a54720a5f8343acf8874d00e891403e1
Alex Pasmantier <alex.pasmant@gmail.com>
2025-12-14 18:46:12 +0100

    perf(preview): add preview bench

--- benches/main.rs
+++ benches/main.rs
@@ -2,6 +2,7 @@ use criterion::criterion_main;
 
 pub mod main {
     pub mod load_candidates;
+    pub mod previewer;
     pub mod render;
     pub mod strings;
     pub mod ui;
@@ -12,6 +13,7 @@ pub use main::*;
 criterion_main!(
     ui::benches,
     load_candidates::benches,
+    previewer::benches,
     render::benches,
     strings::benches,
 );
