8d8b5ba51b516455d3e81b3b9de7044fa3298b15
Osma S. Rautila <osma.rautila@helsinki.fi>
2026-01-08 15:13:29 +0200

    add tests for braille strings

--- television/utils/strings.rs
+++ television/utils/strings.rs
@@ -1,6 +1,6 @@
 use std::borrow::Cow;
 
-use lazy_regex::{Lazy, Regex, regex};
+use lazy_regex::{regex, Lazy, Regex};
 
 use crate::screen::result_item::ResultItem;
 
@@ -485,7 +485,7 @@ pub fn replace_non_printable_bulk<'a>(
                     // Braille Unicode characters
                     c if ('\u{2800}'..='\u{28FF}').contains(&c) => {
                         output.push(c);
-                    },
+                    }
                     // Unicode characters above 0x0700 seem unstable with ratatui
                     c if c > '\u{0700}' => {
                         output.push(NULL_SYMBOL);
@@ -949,6 +949,14 @@ mod tests {
         let (output, offsets) = replace_non_printable_bulk(input, &config);
         assert_eq!(output, "안녕하세요!");
         assert_eq!(offsets, vec![0, 0, 0, 0, 0, 0]);
+    }
+    #[test]
+    fn test_braille_characters() {
+        let input = "⠓⠑⠇⠇⠕⠀⠺⠕⠗⠇⠙";
+        let config = ReplaceNonPrintableConfig::default();
+        let (output, offsets) = replace_non_printable_bulk(input, &config);
+        assert_eq!(output, "⠓⠑⠇⠇⠕⠀⠺⠕⠗⠇⠙");
+        assert_eq!(offsets, vec![0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]);
     }
     #[test]
     fn test_replace_non_printable_no_range_changes() {
