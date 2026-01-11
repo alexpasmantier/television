eefacf62faac6abf4a7dd87f5865057a1dfed6a9
Alex Pasmantier <alex.pasmant@gmail.com>
2026-01-11 14:44:37 +0100

    chore: cleaning up mode color dead code

--- television/screen/mode.rs
+++
@@ -1,9 +0,0 @@
-use crate::{screen::colors::ModeColorscheme, television::Mode};
-use ratatui::style::Color;
-
-pub fn mode_color(mode: Mode, colorscheme: &ModeColorscheme) -> Color {
-    match mode {
-        Mode::Channel => colorscheme.channel,
-        Mode::RemoteControl => colorscheme.remote_control,
-    }
-}
